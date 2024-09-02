b5463d02ea86901d2d4ada63cd6e1edbcf9c6456
Kristen Swanson <kristennicoleswanson@gmail.com>
2024-09-02 12:12:18 -0700

    Initial Commit

---
+++ inkwellpoetry/inkwellpoetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  inkwellpoetryApp.swift
+//  inkwellpoetry
+//
+//  Created by Kristen Swanson on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct inkwellpoetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
